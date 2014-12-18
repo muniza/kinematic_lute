c13e45b91e6365872f1afda2da17c67e1264b222
nlab <nlabarbera507@gmail.com>
2014-12-18 00:22:34 -0500

    Fixed light show code

--- lights/lute_lightshow.ino
+++ lights/lute_lightshow.ino
@@ -41,7 +41,6 @@ void loop() {
   //play triggered light show
   startShow(state);
   }
-}
 
 void startShow(int i) {
   switch(i){
@@ -102,15 +101,20 @@ void colorWipe2(uint32_t c, uint8_t wait) {
 }
 
 //rainbow show
-void rainbow(uint8_t wait) {
+void rainbowCycle(uint8_t wait) {
   uint16_t i, j;
 
-  for(j=0; j<256; j++) {
-    for(i=0; i<strip.numPixels(); i++) {
-      strip.setPixelColor(i, Wheel((i+j) & 255));
+  for(j=0; j<256*5; j++) { // 5 cycles of all colors on wheel
+    for(i=0; i< strip.numPixels(); i++) {
+      strip.setPixelColor(i, Wheel(((i * 256 / strip.numPixels()) + j) & 255));
     }
     strip.show();
     delay(wait);
+    
+    if ((flag == 1)) {
+        colorWipe(strip.Color(0, 0, 0), 1);
+        break;
+    }
   }
 }
 
